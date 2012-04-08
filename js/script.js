c33eecb57806e01fa5c6c02ac9d8d0a21b38ac57
Paul Swartz <paulswartz@gmail.com>
2012-04-08 18:41:16 -0400

    branches, but doesn't look the best

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
             'slug': 'blank project',
             'last_build_status': -1,
             'builds': null,
+            'branches': null,
         },
         _travisJSON: function(extra, success) {
             var url = travisURL(this.get('slug') + extra);
@@ -26,29 +27,39 @@ $(document).ready(function () {
             xhr = this._travisJSON('', this.save);
             this._travisJSON('/builds', function(data) {
                 builds = this.get('builds') || {};
+                branches = this.get('branches') || {};
                 _.each(data, function(build) {
-                    builds[build.id] = build;
+                    branch_builds = branches[build.branch] || {};
+                    branch_builds[build.id] = builds[build.id] = build;
+                    branches[build.branch] = branch_builds;
                 });
                 this.save({builds: builds,
-                          num_builds: _.size(builds)});
+                           branches: branches,
+                           num_builds: _.size(builds)});
             });
             return xhr;
         },
-        trendData: function() {
-            return _.chain(this.get('builds')).values().sortBy(function(build) {
+        trendData: function(branch) {
+            var builds = null;
+            if (typeof branch === 'undefined') {
+                builds = this.get('builds');
+            } else {
+                builds = this.get('branches')[branch];
+            }
+            return _.chain(builds).values().sortBy(function(build) {
                 return build.id;
             }).last(10).map(function(build) {
                 return build.result ? -1 : 1;
             }).value();
         }
     });
-    
+                                        
     var ProjectList = Backbone.Collection.extend({
         model: Project,
         localStorage: new Store("travis-projects"),
         update: function() {
             this.each(function(project) {
-                project.loadFromTravis.call(project);
+                project.loadFromTravis(project);
             });
         }
     });
@@ -58,11 +69,13 @@ $(document).ready(function () {
     var ProjectView = Backbone.View.extend({
         tagName: 'tr',
         template: _.template($('#project-row').html()),
+        branchProjectTemplate: _.template($("#branch-project-row").html()),
         events: {
             'click .delete': 'clear',
+            'click': 'toggleBranches',
         },
         initialize: function() {
-            _.bindAll(this, 'render', 'clear', 'remove');
+            _.bindAll(this, 'render', 'clear', 'remove', 'toggleBranches');
             this.model.bind('change', this.render);
             this.model.bind('destroy', this.remove);
 
@@ -70,19 +83,40 @@ $(document).ready(function () {
         render: function() {
             this.$el.html(this.template(this.model.toJSON())).addClass('project').addClass(
                 this.model.get('last_build_status') ? 'red': 'green');
-            var trendData = this.model.trendData();
-            this.$el.find('.sparkline').sparkline(
-                this.model.trendData(),
-                {type: 'tristate',
+            var sparklineOptions = {
+                type: 'tristate',
                 posBarColor: "green",
-                negBarColor: "red"});
+                negBarColor: "red"};
+            var trendData = this.model.trendData();
+            this.$('.sparkline').sparkline(
+                this.model.trendData(),
+                sparklineOptions);
+            var branch_list = this.$('.project .branches').hide();
+            var model = this.model;
+            var branches = this.model.get('branches');
+            if (branches !== null) {
+                var projectTemplate = this.branchProjectTemplate;
+                var sparklineTemplate = this.branchSparklineTemplate;
+                _.chain(branches).keys().each(function (branch) {
+                    branchTrend = model.trendData(branch);
+                    $("<li/>").html(projectTemplate({'branch': branch})).addClass(
+                        _.last(branchTrend) == 1 ? 'green' : 'red').appendTo(branch_list).children('.sparkline').sparkline(
+                            branchTrend,
+                            sparklineOptions);
+                });
+            }
             return this;
+        },
+        toggleBranches: function() {
+            this.$el.find('.branches').toggle();
+            console.log(this.$el.find('.branches'));
+            $.sparkline_display_visible();
         },
         clear: function() {
             this.model.destroy();
         },
     });
-    
+
     var TravisMonitorView = Backbone.View.extend({
         el: $('#container'),
         events: {
@@ -113,7 +147,7 @@ $(document).ready(function () {
                 project.collection = Projects;
                 this.input.val('');
                 project.loadFromTravis().success(function() {
-                    Projects.add([project]);
+                    Projects.add(project);
                 });
             }
         },
